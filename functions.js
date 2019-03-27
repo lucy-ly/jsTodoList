2e5f7b36a61689fa669114f37399cd81f3dcbf87
Lucy Ly <lucyly.student@careerdevs.com>
2019-03-27 23:30:40 +0000

    updated version02

--- functions.js
+++ functions.js
@@ -1,1 +1,30 @@
-// functions below
+var todos = ['item 1', 'item 2', 'item 3'];
+
+// It should have a function to display todos.
+function displayTodos() {
+    console.log('My todos:', todos);
+}
+
+// It should have a function to add todos.
+function addTodo(todo) { //addTodo('new todo')
+    todos.push(todo);
+    displayTodos();
+}
+
+// It should have a function to change todos.
+function changeTodo(position, newValue) {
+    todos[position] = newValue; 
+    displayTodos();
+}
+
+// It should have a function to delete todos.
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+//CALL and TEST Functions
+displayTodos();
+deleteTodo(2);
+changeTodo(0, 'item 1a');
+addTodo('item 4');
