b17ae2b27bc4d363c091f8469281eb3b30027876
Lucy Ly <lucyly.student@careerdevs.com>
2019-03-30 14:06:37 +0000

    commit v.3

---
+++ htmlDOM.js
@@ -0,0 +1,17 @@
+//HTML essentials, describes webpages
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel=“stylesheet” href=“styles.css”> //does not have a closing tag
+        <script src="script.js"></script>
+    </head>
+
+    <body>
+        <h1>Hello World!</h1>
+        <h2>Subsection title</h2>
+        <h3>Subsection title</h3>
+        
+        </body>
+
+</html>
